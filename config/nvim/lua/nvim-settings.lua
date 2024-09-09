f7cc4bd4c42a781189e00aaa05df34f273fda7fd
Tim Schippers <144120458+TimSchippers@users.noreply.github.com>
2024-09-09 10:40:31 +0200

    Add highlighting on yanking

--- config/nvim/lua/nvim-settings.lua
+++ config/nvim/lua/nvim-settings.lua
@@ -49,4 +49,13 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt.expandtab = false
 	end,
 })
-vim.opt.clipboard = "unnamed"
+
+-- highlight when yanking
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+  vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+      vim.highlight.on_yank()
+    end,
+  group = highlight_group,
+  pattern = '*',
+})
