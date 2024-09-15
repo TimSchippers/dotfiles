0930e68d9c67fea596fd13ebce21eaa42db3b8b6
Tim Schippers <144120458+TimSchippers@users.noreply.github.com>
2024-09-15 12:07:53 +0200

    formatWezterm.lua

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,29 +7,28 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- appearance config
-config.color_scheme = 'Gruvbox Material (Gogh)'
+config.color_scheme = "Gruvbox Material (Gogh)"
 
-config.font = wezterm.font 'mononoki'
+config.font = wezterm.font("mononoki")
 
 config.window_decorations = "RESIZE"
 config.window_padding = {
-  left = 2,
-  right = 2,
-  top = 2,
-  bottom = 0,
+	left = 2,
+	right = 2,
+	top = 2,
+	bottom = 0,
 }
 config.hide_tab_bar_if_only_one_tab = true
-
 
 config.audible_bell = "Disabled"
 
 -- disable making tabs because i always use tmux
 config.keys = {
-  {
-    key = 't',
-    mods = 'CMD',
-    action = wezterm.action.DisableDefaultAssignment,
-  },
+	{
+		key = "t",
+		mods = "CMD",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
 }
 
 -- and finally, return the configuration to wezterm
